960f63a8ae5fd7738e21c3c70fdaaade8c2ec5ad
jrwyman <jacobwyman1995@gmail.com>
2021-02-19 02:35:59 -0700

    deployment test

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
@@ -13,10 +14,11 @@ const P2PClient = new PubSub(ElementProtocol);
 const DEFAULT_PORT = 5000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
-const pub1 = '0433e377e61e04af342dac7e2b273b5c331c128ace089e89cf56b4da15090eb39334abe9620c32ac950b8a417e57187506a9ff45d05bd8963e70819382c21b0a3e';
-const priv1 = '075b1b1702fc0e10cd1b780c030116bc5e08c99dbaa35ed4768bc238b7df5841';
-
 app.use(bodyParser.json());
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/explore', (req, res) => {
   res.json(ElementProtocol);
@@ -58,6 +60,10 @@ let PEER_PORT;
 if (process.env.GENERATE_PEER_PORT === 'true') {
   PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = (PEER_PORT || process.env.PORT || 5000);
 app.listen(PORT, () => {
